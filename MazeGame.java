af240694b731618c4f06ac7b8c0352d930dcfdd2
BenavidesS921 <benavides.s921446@stu.sanjac.edu>
2025-11-29 19:36:37 -0600

    Create MazeGame.java

---
+++ MazeGame.java
@@ -0,0 +1,36 @@
+import java.io.*;
+public class MazeGame 
+{
+  public static void main(String[] args)
+  {
+    //read maze.dat file
+    try (BufferedReader br = new BufferedReader(new FileReader("maze.dat")))
+      {
+        while(true)
+          {
+            String[] block = new String[20];
+            //read 20 lines
+            int counter = 0;
+            while(count < 20)
+              {
+                String line = br.readLine();
+                if(line == null)
+                {
+                  return;
+                }
+                if(line.isEmpty())
+                {
+                  continue;
+                }
+                block[counter++] = line
+              }
+            Maze m = new Maze(block); //creates maze object
+            System.out.println(m.printBlock()); //prints maze
+            System.out.println(m.toString()); //prints Maze:YES or Maze:NO
+            System.out.println();
+          }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+  }
+}
